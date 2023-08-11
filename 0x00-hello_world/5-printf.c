8f074c1402c7ff86b1f188ce149f7badd5fe4e3e
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2023-08-10 20:15:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program printing a line with the use of printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
