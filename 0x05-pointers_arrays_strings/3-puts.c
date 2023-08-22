851a4d51d21cc516724b0f462ca10116ba2f6b7f
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2023-08-21 17:52:33 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string
+ * @str: String printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
