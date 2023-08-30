baea60f5da5b97a7a17b610bb9fa14c14413ac3b
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2023-08-30 15:46:36 -0700

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function searches for a set of bytes in a string
+ * @s: string to search from
+ * @accept: string to search in s
+ * Return: accept or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		const char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
