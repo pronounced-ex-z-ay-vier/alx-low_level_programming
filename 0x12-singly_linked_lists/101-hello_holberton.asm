4bcf4db94ac82def91192ad35ab9e7e9f4ca6760
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-02-01 21:11:33 -0800

    hello-holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+	extern	printf		; the C function, to be called
+		section .data		; Data section, the initialized variable
+msg:		db "Hello, Holberton", 0 ; C string needs 0
+fmt:		db "%s", 10, 0		; The printf format, "\n", '0'
+
+		section .text		; Code section.
+
+		global main		; the standard gcc entry point
+main:					; the program label for the entry point
+		push	rbp		; set up stack frame, must be alligned
+
+		mov	rdi,fmt
+		mov	rsi,msg
+		mov	rax,0		; or it can be xor rax,rax
+		call	printf		; Call C function
+
+		pop	rbp		; restore stack
+		mov	rax,0		; normal, no error, return value
+		ret			; return
