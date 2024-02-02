3617d3b2377d793d47d18c38a9553a2456d2dadb
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-02-01 20:50:06 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - constructor attribute to myStartupFun to execute before main
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
