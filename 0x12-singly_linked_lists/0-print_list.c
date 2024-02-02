caf7295ede8518c2b07c8dfa5e44719da1e849f3
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-02-01 19:41:27 -0800

    print-list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - Prints elements of the list_t list
+ * @h: Singly linked list
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
