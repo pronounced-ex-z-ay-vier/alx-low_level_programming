113df575fc2b13e31f1f28681bb48a04d3b29fa0
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-02-07 05:19:41 -0800

    101-m

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,6 +19,7 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
