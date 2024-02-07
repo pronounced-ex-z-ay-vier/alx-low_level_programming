c37e553ab9d039093a12eedb346ffb41bf7f3dfb
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-02-06 18:57:36 -0800

    0-a

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of listint list
+ * @h: Head of link node
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+
+re singly lists
