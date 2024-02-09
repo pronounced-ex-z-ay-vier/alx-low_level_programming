c397158c550802f78dfd10216c4a054845464699
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-02-08 20:39:32 -0800

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int SB;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-			return (-1);
+		return (-1);
 	SB = 1 << index;
 	*n = *n | SB;
 	return (1);
